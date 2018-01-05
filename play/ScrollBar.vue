92012ae771e931fd9314f9a29159e53c83e70386
katashin <ktsn55@gmail.com>
2018-01-05 21:33:54 +0900

    feat: add padding right with scrollbar width during modal is open (#2)

---
+++ play/ScrollBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <button @click="open('test')">Open</button>
+    <p class="float">
+      This should not move during opening/closing a modal
+    </p>
+    <modal name="test">
+      <div class="basic-modal">
+        <h1 class="title">Modal Title</h1>
+        <button class="button" @click="close">Close</button>
+      </div>
+    </modal>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    open (name) {
+      this.$modal.push(name)
+    },
+
+    close () {
+      this.$modal.pop()
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  overflow-y: scroll;
+}
+
+.float {
+  float: right;
+}
+</style>
