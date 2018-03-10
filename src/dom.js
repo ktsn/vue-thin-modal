8d5b8074b3bda9f818c3012aacc0c7125a23fa87
ktsn <ktsn55@gmail.com>
2018-03-10 12:56:42 +0900

    refactor: remove redundant check

--- src/dom.js
+++ src/dom.js
@@ -1,35 +1,34 @@
-// @flow
 /* eslint-env browser */
 
 const isBrowser = typeof window !== undefined
 
 export function appendToBody (el: HTMLElement): void {
-  if (isBrowser && document.body) {
+  if (isBrowser) {
     document.body.appendChild(el)
   }
 }
 
 export function addBodyClass (className: string): void {
-  if (isBrowser && document.body) {
+  if (isBrowser) {
     document.body.classList.add(className)
   }
 }
 
 export function removeBodyClass (className: string): void {
-  if (isBrowser && document.body) {
+  if (isBrowser) {
     document.body.classList.remove(className)
   }
 }
 
 export function setBodyCss (name: string, value: string): void {
-  if (isBrowser && document.body) {
+  if (isBrowser) {
     const s: any = document.body.style
     s[name] = value
   }
 }
 
 export function getScrollBarWidth (): ?number {
-  if (!isBrowser || !document.documentElement) {
+  if (!isBrowser) {
     return null
   }
   return window.innerWidth - document.documentElement.offsetWidth
