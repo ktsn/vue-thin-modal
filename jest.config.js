d406f3d7d2940dd28c1305b2804638e76a749714
ktsn <ktsn@hey.com>
2021-06-21 11:38:54 +0800

    chore: bump deps

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   testRegex: '/test/.+\\.spec\\.js$',
   moduleNameMapper: {
-    '^vue$': 'vue/dist/vue.common.js'
+    '^vue$': 'vue/dist/vue.common.js',
   },
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ]
+  snapshotSerializers: ['jest-serializer-vue'],
 }
